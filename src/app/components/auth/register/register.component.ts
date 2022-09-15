8cba6ee4cd83bd3fff84f59d786254c9acc3c7a1
tanersaydam <tanersaydam@gmail.com>
2022-09-15 10:21:22 +0300

    .

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -8,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   img: any;
+  list: string[] = [
+    "List 1",
+    "List 2",
+    "List 3",
+    "List 4",
+    "List 5",
+    "List 6",
+    "List 7",
+  ]
+
+  selectedItem: string = "List 1";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setImage(img: HTMLInputElement){
+    //console.log(img.files?[0]): undefined
+  }
+
+  setSelectedItem(event: any){
+    console.log(event.target.selected.value);  
+  }
+
   signUp(){
-    console.log(this.img)
+    console.log(this.selectedItem)
   }
 
 }
